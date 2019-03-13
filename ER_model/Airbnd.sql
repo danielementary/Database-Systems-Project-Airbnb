63cf9071b5f2f2e2e1f0ec6fa6d402cf45eff9fb
Samuel Chassot <samuel.chassot@epfl.ch>
2019-03-13 12:55:48 +0100

    add pdf and sql for the ER

---
+++ ER_model/Airbnd.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Listing {
+  id INTEGER,
+  listing_url CHAR(50),
+  name CHAR(50),
+  summary CHAR(50),
+  space CHAR(50),
+  description TEXT,
+  notes TEXT,
+  transit TEXT,
+  access TEXT,
+  interaction TEXT,
+  picture_url CHAR(50),
+  
+}
