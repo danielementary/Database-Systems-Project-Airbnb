a5a6f73025d328d0bf3fc8cc95317b76cc9de185
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-03-21 18:29:50 +0100

    Checkout Airbnb

--- Database/Airbnd.sql
+++ Database/Airbnd.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE Airbnb;
 
 ----------------Entities-------------------
 
@@ -15,20 +16,15 @@ CREATE TABLE Listing (
   listing_access      TEXT,
   listing_interaction TEXT,
   listing_picture_url VARCHAR(50),
+  neighbourhood_overview TEXT,
 
   -----relation attributes----
   --a listing is owned by a host--
   host_id INT NOT NULL,
-  --a listing is in a country--
-  city_name VARCHAR(50) NOT NULL,
-  --a listing is in a neighbourhood--
-  neighbourhood_id INT NOT NULL,
 
   ------------keys------------
   PRIMARY KEY(id),
   FOREIGN KEY(host_id) REFERENCES Host(host_id),
-  FOREIGN KEY(city) REFERENCES City(city_name),
-  FOREIGN KEY(neighbourhood_id) REFERENCES Neighbourhood(neighbourhood_id)
 );
 
 CREATE TABLE Host (
@@ -56,14 +52,13 @@ CREATE TABLE Host (
 CREATE TABLE Neighbourhood (
 
   ---------attributes---------
-  neighbourhood_id INT AUTO_INCREMENT,
   neighbourhood_name VARCHAR(50),
-  neighbourhood_overview TEXT,
-
   -----relation attributes----
-
+  city_name    VARCHAR(50),
+  country_code INT,
   ------------keys------------
-  PRIMARY KEY(id)
+  PRIMARY KEY(neighbourhood_id)
+  FOREIGN KEY(city_name, country_code) REFERENCES City
 );
 
 CREATE TABLE City (
@@ -76,7 +71,7 @@ CREATE TABLE City (
   -----relation attributes----
 
   ------------keys------------
-  PRIMARY KEY(city_name)
+  PRIMARY KEY(city_name, country_code)
 );
 
 CREATE TABLE House_properties (
@@ -166,7 +161,7 @@ CREATE TABLE Reviewer (
   PRIMARY KEY(reviewer_id),
 );
 
-CREATE TABLE Review_scores (
+CREATE TABLE review_scores (
 
   ---------attributes---------
   review_scores_rating        FLOAT,
@@ -175,7 +170,6 @@ CREATE TABLE Review_scores (
   review_scores_checkin       FLOAT,
   review_scores_communication FLOAT,
   review_scores_location      FLOAT,
-  review_scores_checkin       FLOAT,
   review_scores_value         FLOAT,
 
   -----relation attributes----
@@ -211,8 +205,10 @@ CREATE TABLE Location (
 
   -----relation attributes----
   listing_id INT,
+  neighbourhood_id INT NOT NULL,
 
   ------------keys------------
   PRIMARY KEY(listing_id),
-  FOREIGN KEY(listing_id) REFERENCES Listing(listing_id)
+  FOREIGN KEY(listing_id) REFERENCES Listing(listing_id),
+  FOREIGN KEY(neighbourhood_id) REFERENCES Neighbourhood
 );
