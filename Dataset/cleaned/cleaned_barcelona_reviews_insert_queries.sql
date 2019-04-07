b5d78b9444f4bd6546d51940e722ad671f1ea2bb
Samuel Chassot <samuel.chassot@epfl.ch>
2019-04-07 18:06:58 +0200

    reviews should be cleaned, create sql insert files for reviews

---
+++ Dataset/cleaned/cleaned_barcelona_reviews_insert_queries.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:da25c1fabb20801f403d675c55b6c85e00584e53687be202910d43a0a7712830
+size 178865075
---
+++ Dataset/cleaned/cleaned_berlin_reviews_insert_queries.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b732359ffdd021755eea74901e10fbf28a7a49897fc99b6d07f3c853ea3420a9
+size 111573052
