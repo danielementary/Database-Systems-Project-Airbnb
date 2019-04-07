83f4dd69d07dc46b57a45fcfcc22482e713b0501
Samuel Chassot <samuel.chassot@epfl.ch>
2019-04-07 18:06:58 +0200

    reviews should be cleaned, create sql insert files for reviews

---
+++ Dataset/cleaned/cleaned_madrid_reviews_insert_queries.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2f44d6eed2009a272c916abbdae2e4d3daa1eb5000b9cabf5438f1547dd4437
+size 164064434
--- Parser/clean_and_insert_reviews.py
+++ Parser/clean_and_insert_reviews.py
@@ -21,18 +21,58 @@ def clean_reviews_data(filename):
     #save the cleaned version back
 
     splited_original_name = filename.split('/')
-    newname = '/'.join(splited_original_name[:-1]) + "/cleaned_" + splited_original_name[-1:][0]
+    newname = '/'.join(splited_original_name[:-1]) + "/cleaned/cleaned_" + splited_original_name[-1:][0]
     data_frame.to_csv(newname, encoding='utf8')
     print("new cleaned version of the file has been saved under name : " + newname)
+
+    file.close()
+    return newname
 
 def addQuotes(string):
     string = str(string)
     return string
 
 
+def insert_reviews(filename):
+    """
+    write a .sql file containing INSERT statements required to insert data contained in filename
+    """
+    file = open(filename, newline='')
+    data_frame = pd.read_csv(filename, dtype={'listing_id': int, 'id': int, 'reviewer_id': int, 'reviewer_name': str, 'comments': str})
+
+    queries = []
+
+    for index, row in data_frame.iterrows():
+        #here get columns and create sql query
+        #columns are in order: review_id, review_date, review_comments, reviewer_id, listing_id
+        sql_query = """INSERT INTO review VALUES ({}, {}, {}, {}, {})""".format(
+                row['id'],
+                row['date'],
+                '"' + str(row['comments']) + '"',
+                row['reviewer_id'],
+                row['listing_id']
+        )
+        queries.append(sql_query)
+
+
+    splited_original_name = filename.split('/')
+    outname = '/'.join(splited_original_name[:-1]) + '/' + splited_original_name[-1:][0].split('.')[0] + "_insert_queries.sql"
+    outputfile = open(outname, 'w')
+
+    for q in queries:
+        outputfile.write(q + '\n')
+
+    print("insert queries have been written for (", filename, ") in : ", outname)
+    outputfile.close()
+
 reviews_files = ["barcelona_reviews.csv", "berlin_reviews.csv", "madrid_reviews.csv"]
 
 reviews_files = ["../Dataset/" + file for file in reviews_files]
 
+cleaned_files = []
+
 for file in reviews_files:
-    clean_reviews_data(file)
+    cleaned_files.append(clean_reviews_data(file))
+
+for file in cleaned_files:
+    insert_reviews(file)
