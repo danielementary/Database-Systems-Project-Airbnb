86dde5ecf8af787ee1f2d2fb444e4f22afb59fad
Samuel Chassot <samuel.chassot@epfl.ch>
2019-04-08 09:41:30 +0200

    create lists and dictionaries to automate the attributes processing

--- Parser/clean_and_insert_listings.py
+++ Parser/clean_and_insert_listings.py
@@ -6,7 +6,12 @@ def clean_listings_data(filename):
     file = open(filename, newline='')
     data_frame = pd.read_csv(filename)
 
-    print(data_frame.columns)
+    string_attributes = ['url', 'name', 'summary']
+
+    columns = data_frame.columns.tolist()
+
+    data_frame['neighbourhood'] = data_frame['neighbourhood'].apply(addQuotes)
+    print(data_frame['neighbourhood'].tolist())
 
 
 def create_insert_queries(filename):
@@ -17,15 +22,25 @@ def create_insert_queries(filename):
                 "Economic_properties", "Administrative_properties",\
                 "Review_scores", "City", "Location"]
 
+    #city of host_neighbourhood: say for now that is the same as the listing's
+    # don't forget to add neighbourhood to the Table when insert host and listing
+    # need to keeps tracks of neighbourhood already added
+    #NEIGHBOURHOOD is special so does NOT have a dictionary
     tables_to_attributes = \
-        {"Listing": ["listing_id", "listing_url", "listing_name", "listing_summary", "listing_space", "listing_description", "listing_notes", "listing_transit", "listing_access", "listing_interaction", "host_id"],\
-         "Host": ["host_id", "host_url"", ""host_name", "host_since", "host_about", "host_response_time", "host_response_rate", "host_thumbnail_url", "host_picture_url", "host_verifications", "neighbourhood_name", "city_name"],\
+        {"Listing": {"listing_id": "id", "listing_url": "listing_url", "listing_name": "name", "listing_summary": "summary", "listing_space": "space", "listing_description": "description", "listing_notes": "notes", "listing_transit": "transit", "listing_access": "access", "listing_interaction": "interaction","listing_picture_url": "picture_url", "listing_neighbourhood_overview" : "neighbourhood_overview", "host_id": "host_id"},\
+         "Host": {"host_id" : "host_id", "host_url" : "host_url", "host_name" : "host_name", "host_since" : "host_since", "host_about" : "host_about", "host_response_time" : "host_response_time", "host_response_rate" : "host_response_rate", "host_thumbnail_url" : "host_thumbnail_url", "host_picture_url" : "host_picture_url", "host_verifications" : "host_verifications", "neighbourhood_name": "host_neighbourhood", "city_name": "city"},\
          "Neighbourhood": ["neighbourhood_name", "city_name", "country_code"],\
-         "House_properties": ["property_type", "room_type", "accomodates", "bathrooms", "bedrooms", "beds", "bed_type", "amenities", "square_feet", "listing_id"], \
-         "Economic_properties": ["price", "weekly_price", "monthly_price", "security_deposit", "cleaning_fee", "guests_included", "extra_people", "listing_id"],\
-         "Administrative_properties": ["rules", "minimum_nights", "maximum_nights", "is_business_travel_ready", "cancellation_policy", "require_guest_profile_picture", "require_guest_phone_verification", "listing_id"],\
-         "Review_scores": ["review_scores_rating", "review_scores_accuracy", "review_scores_cleanliness", "review_scores_checkin", "review_scores_communication", "review_scores_location", "review_scores_value", "listing_id"],\
-         "City": ["city_name", "country_code", "country",\
-         "Location": ["latitude", "longitude", "listing_id", "neighbourhood_name", "city_name"]]}
+         "House_properties": {"property_type": "property_type", "room_type": "room_type", "accomodates": "accomodates", "bathrooms": "bathrooms", "bedrooms": "bedrooms", "beds": "beds", "bed_type": "bed_type", "amenities":  "amenities", "square_feet": "square_feet", "listing_id": "id"}, \
+         "Economic_properties": {"price": "price", "weekly_price": "weekly_price", "monthly_price": "monthly_price", "security_deposit": "security_deposit", "cleaning_fee": "cleaning_fee", "guests_included": "guests_included", "extra_people": "extra_people", "listing_id": "id"},\
+         "Administrative_properties": {"rules": "house_rules", "minimum_nights": "minimum_nights", "maximum_nights": "maximum_nights", "is_business_travel_ready": "is_business_travel_ready", "cancellation_policy":  "cancellation_policy", "require_guest_profile_picture": "require_guest_profile_picture", "require_guest_phone_verification": "require_guest_phone_verification", "listing_id": "id"},\
+         "Review_scores": {"review_scores_rating": "review_scores_rating", "review_scores_accuracy": "review_scores_accuracy", "review_scores_cleanliness": "review_scores_cleanliness", "review_scores_checkin": "review_scores_checkin", "review_scores_communication": "review_scores_communication", "review_scores_location": "review_scores_location", "review_scores_value": "review_scores_value", "listing_id": "id"},\
+         "City": {"city_name": "city", "country_code": "country_code", "country": "country",\
+         "Location": {"latitude": "latitude", "longitude": "longitude", "listing_id": "id", "neighbourhood_name": "neighbourhood", "city_name": "city"}}}
+
+
+
+def addQuotes(string):
+    string = str(string)
+    return string
 
 clean_listings_data("../Dataset/barcelona_listings.csv")
