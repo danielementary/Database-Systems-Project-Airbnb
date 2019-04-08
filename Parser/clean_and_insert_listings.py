d08f242bc90111565975664e8b2713c9876b514d
Samuel Chassot <samuel.chassot@epfl.ch>
2019-04-08 07:15:49 +0200

    populate tables_to_attributes that stores every table involved in listing and its attributes

--- Parser/clean_and_insert_listings.py
+++ Parser/clean_and_insert_listings.py
@@ -20,6 +20,12 @@ def create_insert_queries(filename):
     tables_to_attributes = \
         {"Listing": ["listing_id", "listing_url", "listing_name", "listing_summary", "listing_space", "listing_description", "listing_notes", "listing_transit", "listing_access", "listing_interaction", "host_id"],\
          "Host": ["host_id", "host_url"", ""host_name", "host_since", "host_about", "host_response_time", "host_response_rate", "host_thumbnail_url", "host_picture_url", "host_verifications", "neighbourhood_name", "city_name"],\
-         "Neighbourhood": []}
+         "Neighbourhood": ["neighbourhood_name", "city_name", "country_code"],\
+         "House_properties": ["property_type", "room_type", "accomodates", "bathrooms", "bedrooms", "beds", "bed_type", "amenities", "square_feet", "listing_id"], \
+         "Economic_properties": ["price", "weekly_price", "monthly_price", "security_deposit", "cleaning_fee", "guests_included", "extra_people", "listing_id"],\
+         "Administrative_properties": ["rules", "minimum_nights", "maximum_nights", "is_business_travel_ready", "cancellation_policy", "require_guest_profile_picture", "require_guest_phone_verification", "listing_id"],\
+         "Review_scores": ["review_scores_rating", "review_scores_accuracy", "review_scores_cleanliness", "review_scores_checkin", "review_scores_communication", "review_scores_location", "review_scores_value", "listing_id"],\
+         "City": ["city_name", "country_code", "country",\
+         "Location": ["latitude", "longitude", "listing_id", "neighbourhood_name", "city_name"]]}
 
 clean_listings_data("../Dataset/barcelona_listings.csv")
