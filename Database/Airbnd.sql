38f23f42c8189e996ac0febea32cba85ecbd4d46
Samuel Chassot <samuel.chassot@epfl.ch>
2019-04-12 15:14:48 +0200

    remove string keys remove small one to one table

--- Database/Airbnd.sql
+++ Database/Airbnd.sql
@@ -18,12 +18,56 @@ CREATE TABLE Listing (
   listing_picture_url VARCHAR(50),
   listing_neighbourhood_overview TEXT,
 
+  -- House_properties
+  property_type VARCHAR(50),
+  room_type     VARCHAR(50),
+  accommodates  TINYINT,
+  bathrooms     TINYINT,
+  bedrooms      TINYINT,
+  beds          TINYINT,
+  bed_type      VARCHAR(50),
+  amenities     TEXT,
+  square_feet   SMALLINT,
+
+  -- Economic_properties
+  price            FLOAT,
+  weekly_price     FLOAT,
+  monthly_price    FLOAT,
+  security_deposit FLOAT,
+  cleaning_fee     FLOAT,
+  guests_included  TINYINT,
+  extra_people     FLOAT,
+
+  -- Administrative_properties
+  rules          TEXT,
+  minimum_nights INT,
+  maximum_nights INT,
+  is_business_travel_ready BIT,
+  cancellation_policy      TEXT,
+  require_guest_profile_picture    BIT,
+  require_guest_phone_verification BIT,
+
+  -- Review_scores
+  review_scores_rating        FLOAT,
+  review_scores_accuracy      FLOAT,
+  review_scores_cleanliness   FLOAT,
+  review_scores_checkin       FLOAT,
+  review_scores_communication FLOAT,
+  review_scores_location      FLOAT,
+  review_scores_value         FLOAT,
+
+  -- Location
+  latitude         FLOAT,
+  longitude        FLOAT,
+
   -----relation attributes----
-  host_id INT NOT NULL,
+  host_id           INT NOT NULL,
+  neighbourhood_id  INT NOT NULL,
 
   ------------keys------------
   PRIMARY KEY(id),
-  FOREIGN KEY(host_id) REFERENCES Host(host_id)
+  FOREIGN KEY(host_id) REFERENCES Host(host_id),
+  FOREIGN KEY(neighbourhood_id) REFERENCES Neighbourhood(neighbourhood_id)
 );
 
 CREATE TABLE Host (
@@ -46,12 +90,13 @@ CREATE TABLE Host (
 
   ------------keys------------
   PRIMARY KEY(host_id),
-  FOREIGN KEY(neighbourhood_name, city_name) REFERENCES Neighbourhood(neighbourhood_name, city_name)
+  FOREIGN KEY(neighbourhood_id) REFERENCES Neighbourhood(neighbourhood_id)
 );
 
 CREATE TABLE Neighbourhood (
 
   ---------attributes---------
+  neighbourhood_id   INT,
   neighbourhood_name VARCHAR(50),
 
   -----relation attributes----
@@ -59,86 +104,8 @@ CREATE TABLE Neighbourhood (
   country_code VARCHAR(4),
 
   ------------keys------------
-  PRIMARY KEY(neighbourhood_name, city_name)
-  FOREIGN KEY(city_name, country_code) REFERENCES City(city_name, country_code) ON DELETE CASCADE
-);
-
-CREATE TABLE House_properties (
-
-  ---------attributes---------
-  property_type VARCHAR(50),
-  room_type     VARCHAR(50),
-  accommodates  TINYINT,
-  bathrooms     TINYINT,
-  bedrooms      TINYINT,
-  beds          TINYINT,
-  bed_type      VARCHAR(50),
-  amenities     TEXT,
-  square_feet   SMALLINT,
-
-  -----relation attributes----
-  listing_id INT,
-
-  ------------keys------------
-  PRIMARY KEY(listing_id),
-  FOREIGN KEY(listing_id) REFERENCES Listing(listing_id) ON DELETE CASCADE
-);
-
-CREATE TABLE Economic_properties (
-
-  ---------attributes---------
-  price            FLOAT,
-  weekly_price     FLOAT,
-  monthly_price    FLOAT,
-  security_deposit FLOAT,
-  cleaning_fee     FLOAT,
-  guests_included  TINYINT,
-  extra_people     FLOAT,
-
-  -----relation attributes----
-  listing_id INT,
-
-  ------------keys------------
-  PRIMARY KEY(listing_id),
-  FOREIGN KEY(listing_id) REFERENCES Listing(listing_id) ON DELETE CASCADE
-);
-
-CREATE TABLE Administrative_properties (
-
-  ---------attributes---------
-  rules          TEXT,
-  minimum_nights INT,
-  maximum_nights INT,
-  is_business_travel_ready BIT,
-  cancellation_policy      TEXT,
-  require_guest_profile_picture    BIT,
-  require_guest_phone_verification BIT,
-
-  -----relation attributes----
-  listing_id INT,
-
-  ------------keys------------
-  PRIMARY KEY(listing_id),
-  FOREIGN KEY(listing_id) REFERENCES Listing(listing_id) ON DELETE CASCADE
-);
-
-CREATE TABLE Review_scores (
-
-  ---------attributes---------
-  review_scores_rating        FLOAT,
-  review_scores_accuracy      FLOAT,
-  review_scores_cleanliness   FLOAT,
-  review_scores_checkin       FLOAT,
-  review_scores_communication FLOAT,
-  review_scores_location      FLOAT,
-  review_scores_value         FLOAT,
-
-  -----relation attributes----
-  listing_id INT,
-
-  ------------keys------------
-  PRIMARY KEY(listing_id),
-  FOREIGN KEY(listing_id) REFERENCES Listing(listing_id) ON DELETE CASCADE
+  PRIMARY KEY(neighbourhood_id)
+  FOREIGN KEY(city_id) REFERENCES City(city_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Review (
@@ -188,6 +155,7 @@ CREATE TABLE Calendar (
 CREATE TABLE City (
 
   ---------attributes---------
+  city_id      INT,
   city_name    VARCHAR(50),
   country_code VARCHAR(4),
   country      VARCHAR(50),
@@ -195,7 +163,7 @@ CREATE TABLE City (
   -----relation attributes----
 
   ------------keys------------
-  PRIMARY KEY(city_name, country_code)
+  PRIMARY KEY(city_id)
 );
 
 ----------------Relations------------------
@@ -208,11 +176,10 @@ CREATE TABLE Location (
 
   -----relation attributes----
   listing_id         INT,
-  neighbourhood_name VARCHAR(50),
-  city_name          VARCHAR(50),
+  neighbourhood_id   INT,
 
   ------------keys------------
   PRIMARY KEY(listing_id),
   FOREIGN KEY(listing_id) REFERENCES Listing(listing_id) ON CASCADE DELETE,
-  FOREIGN KEY(neighbourhood_name, city_name) REFERENCES Neighbourhood(neighbourhood_name, city_name) ON CASCADE DELETE
+  FOREIGN KEY(neighbourhood_id REFERENCES Neighbourhood(neighbourhood_id) ON CASCADE DELETE
 );
