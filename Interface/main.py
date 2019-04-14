640a791b9d766bd80f82ca63c8b35ca897a0cce4
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-14 22:45:54 +0200

    Close database before destroying app

--- Interface/main.py
+++ Interface/main.py
@@ -1,5 +1,11 @@
 from src.app import App
+from src.database import disconnect
+
+def disconnect_and_close(app):
+    disconnect(app.databaseConnection)
+    app.destroy()
 
 if __name__ == "__main__":
-    App = App()
-    App.mainloop()
+    app = App()
+    app.protocol("WM_DELETE_WINDOW", lambda: disconnect_and_close(app))
+    app.mainloop()
