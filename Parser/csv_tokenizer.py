d0dd43cd5f868656f2fa959ae1abe21ec750c104
Samuel Chassot <samuel.chassot@epfl.ch>
2019-04-19 16:00:28 +0200

    continue working hard, now csv are out, with some problems, added a tokenizer to read csv I output only need to fill with the code to execute for each line

---
+++ Parser/csv_tokenizer.py
@@ -0,0 +1,44 @@
+# this tokenizer is built to read csv files output by the cleaning process,
+# it follows our rules so is absolutely not general purpose
+
+def tokenize(filename):
+    file = open(filename, 'r')
+
+    lines = file.readlines()
+
+    # first extracts columns name:
+    columns = tokenize_line(lines[0])
+    print(columns, "  len = ", len(columns))
+
+    for i in range(1, len(lines)):
+        values = tokenize_line(lines[i])
+
+        # DO SOMETHING WITH EACH LINE HERE
+        print(len(values))
+
+
+
+def tokenize_line(string):
+    string_len = len(string)
+    result = []
+    in_string = False
+    cur_elmt = ""
+    i = 0
+    while i < string_len:
+        c = string[i]
+        if (c == "," or c== "\n") and not in_string:
+            cur_elmt = cur_elmt.replace("\n", "")
+            result.append(cur_elmt)
+            cur_elmt = ""
+        else:
+            if in_string and c == "'" :
+                if string[i+1] == "'":
+                    cur_elmt += "'"
+                    i += 1
+                else:
+                    in_string = False
+            elif not in_string and c == "'":
+                in_string = True
+            cur_elmt += c
+        i += 1
+    return result
