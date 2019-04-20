0b86934e9ebef5f9df2f8afb1dd0dcd9dbdef81f
sophieammann <sophie.ammann@epfl.ch>
2019-04-20 14:40:07 +0200

    query upto 2

--- Database/queries.sql
+++ Database/queries.sql
@@ -1,16 +1,34 @@
 ----------------SQL_Queries-------------------
 
--------01-------
+---------------------01-----------------------
 SELECT AVG(L.price)
-FROM Listing L
+FROM  Listing L
 WHERE L.beds = 8;
 
--------02-------
+---------------------02-----------------------
 SELECT AVG(L.price)
-FROM Listing L
-WHERE L.amenities_id EXISTS ( SELECT A.amenities_id
-                              FROM Amenities A
-                              WHERE A.amenities = ''
+FROM  Listing L, Listing_amenity_map M
+WHERE L.listing_id = M.listing_id
+      AND L.amenities_id EXISTS ( SELECT A.amenities_id
+                                  FROM Amenities A
+                                  WHERE A.amenities = 'TV' --??
+                                );
 
-
-)
+---------------------03-----------------------
+SELECT H.host_name
+FROM  Host H, Listing L
+WHERE H.host_id = L.host_id
+      AND L.host_id EXISTS  ( SELECT C.host_id
+                              FROM Calender C
+                              WHERE C.calendar_available = '1'
+                                    AND C.calendar_date >= 2019-03-01
+                                    AND C.calendar_date < 2019-10-01
+                                    --TODO finish this query 
+                            );
+---------------------04-----------------------
+---------------------05-----------------------
+---------------------06-----------------------
+---------------------07-----------------------
+---------------------08-----------------------
+---------------------09-----------------------
+---------------------10-----------------------
