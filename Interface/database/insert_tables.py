547ffdacfbe6507c5b9cd1f51e0d6c979109ced5
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-22 21:20:05 +0200

    Add file with names of tables to populate

---
+++ Interface/database/insert_tables.py
@@ -0,0 +1,5 @@
+insert_tables_names_ordered = ["Country", "City", "Neighbourhood", "Host",
+                               "Property_type", "Room_type", "Bed_type",
+                               "cancellation_policy", "Listing", "Reviewer",
+                               "Review", "Calendar", "Amenity", "Host_verification",
+                               "Listing_amenity_map", "Host_verification_map"]
