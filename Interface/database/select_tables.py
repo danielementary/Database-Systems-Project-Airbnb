0b606d5b303af1c5c722ff964bb6ffaf24e0e0fb
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-24 18:22:32 +0200

    Add inputs for form and remove host since

--- Interface/database/select_tables.py
+++ Interface/database/select_tables.py
@@ -3,6 +3,6 @@ search_fields = {"Listing": ("listing_name", "accommodates", "square_feet",
                              "price", "is_business_travel_ready",
                              "review_scores_rating", "property_type_id",
                              "cancellation_policy_id"),
-                 "Host" : ("host_name", "host_since"),
+                 "Host" : ("host_name"),
                  "Neighbourhood" : ("neighbourhood_name", "city_id")
                  }
