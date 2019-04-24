2b6873cd8e00c5771eae7631905fb4b5f1cc5175
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-25 00:49:09 +0200

    Add table Day...

--- Interface/database/insert_tables.py
+++ Interface/database/insert_tables.py
@@ -1,5 +1,5 @@
 insert_tables_names_ordered = ["Country", "City", "Neighbourhood", "Host",
                                "Property_type", "Room_type", "Bed_type",
                                "Cancellation_policy", "Listing", "Reviewer",
-                               "Review", "Calendar", "Amenity", "Host_verification",
+                               "Review", "Day", "Calendar", "Amenity", "Host_verification",
                                "Listing_amenity_map", "Host_verification_map"]
