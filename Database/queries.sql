3ca68e588b29fd519d23d534f6a22f9519e886d7
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-26 01:27:05 +0200

    Do query 2 and 3 and refactor

--- Database/queries.sql
+++ Database/queries.sql
@@ -2,29 +2,31 @@
 
 ---------------------01-----------------------
 SELECT AVG(price)
-FROM  Listing
+FROM Listing
 WHERE beds = 8;
 
 ---------------------02-----------------------
 SELECT AVG(L.price)
-FROM  Listing L, Listing_amenity_map M
+FROM  Listing L,
+      Listing_amenity_map M
 WHERE L.listing_id = M.listing_id
-      AND L.amenities_id EXISTS ( SELECT A.amenities_id
-                                  FROM Amenities A
-                                  WHERE A.amenities = 'TV' --??
-                                );
+      AND M.amenity_id = (SELECT A.amenity_id
+                          FROM Amenity A
+                          WHERE A.amenity_name = "TV");
 
 ---------------------03-----------------------
-SELECT H.host_name
-FROM  Host H, Listing L
+SELECT DISTINCT H.host_name
+FROM Host H,
+     Listing L,
+     Day D,
+     Calendar C
 WHERE H.host_id = L.host_id
-      AND L.host_id EXISTS  ( SELECT C.host_id
-                              FROM Calender C
-                              WHERE C.calendar_available = '1'
-                                    AND C.calendar_date >= 2019-03-01
-                                    AND C.calendar_date < 2019-10-01
-                                    --TODO finish this query
-                            );
+      AND L.listing_id = C.listing_id
+      AND C.calendar_available = 1
+      AND C.calendar_day_id = D.day_id
+      AND D.day_date >= "2019-03-01"
+      AND D.day_date < "2019-10-01";
+
 ---------------------04-----------------------
 ---------------------05-----------------------
 ---------------------06-----------------------
