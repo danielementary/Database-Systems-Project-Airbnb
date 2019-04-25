dbdcb5c60c1a83a41fe99a1564f405070e22b104
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-25 02:42:43 +0200

    Select dict for db variables

--- Interface/database/select_tables.py
+++ Interface/database/select_tables.py
@@ -1,4 +1,6 @@
 #available fields for search section of the interface
 search_tables = ("Listing", "Host", "Neighbourhood")
 
-select_city_names_statements = "SELECT city_name, city_id from City;"
+select_property_type_names_ids_statements       = "SELECT property_type_name, property_type_id from Property_type;"
+select_cancellation_policy_names_ids_statements = "SELECT cancellation_policy_name, cancellation_policy_id from Cancellation_policy;"
+select_city_names_ids_statements                = "SELECT city_name, city_id from City;"
