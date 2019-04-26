dc3edb6537ab48a200acfc8d50d95487a6965150
sophieammann <sophie.ammann@epfl.ch>
2019-04-26 14:20:46 +0200

    query 5 implemented

--- Database/queries.sql
+++ Database/queries.sql
@@ -7,7 +7,7 @@ WHERE beds = 8;
 
 ---------------------02-----------------------
 SELECT AVG(L.price)
-FROM  Listing L,
+FROM Listing L,
       Listing_amenity_map M
 WHERE L.listing_id = M.listing_id
       AND M.amenity_id = (SELECT A.amenity_id
@@ -28,7 +28,25 @@ WHERE H.host_id = L.host_id
       AND D.day_date < "2019-10-01";
 
 ---------------------04-----------------------
+SELECT COUNT(L.listing_id)  --TODO check it's right
+FROM Listing L,
+     Host H1,
+     Host H2
+WHERE L.host_id = H1.host_id
+      AND L.host_id = H2.host_id
+      AND
+
 ---------------------05-----------------------
+SELECT DISTINCT D.day_date
+FROM Day D,
+     Calendar C,
+     Listing L,
+     Host H
+WHERE D.day_id = C.calendar_day_id
+      AND C.listing_id = L.listing_id
+      AND C.calendar_available = 1
+      AND L.host_id = H.host_id
+      AND H.host_name = "Viajes Eco";
 ---------------------06-----------------------
 ---------------------07-----------------------
 ---------------------08-----------------------
