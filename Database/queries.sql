085ac2178aa6cb0674795e4873b151ad2c876d7e
sophieammann <sophie.ammann@epfl.ch>
2019-04-28 22:04:36 +0200

    modified query 2
    added smart TV to query2
    query 4 not functionnal

--- Database/queries.sql
+++ Database/queries.sql
@@ -8,11 +8,12 @@ WHERE beds = 8;
 ---------------------02-----------------------
 SELECT AVG(L.price)
 FROM Listing L,
-      Listing_amenity_map M
+     Listing_amenity_map M
 WHERE L.listing_id = M.listing_id
       AND M.amenity_id = (SELECT A.amenity_id
                           FROM Amenity A
-                          WHERE A.amenity_name = "TV");
+                          WHERE A.amenity_name = "TV"
+                          OR "Smart TV");
 
 ---------------------03-----------------------
 SELECT DISTINCT H.host_name
@@ -47,8 +48,31 @@ WHERE D.day_id = C.calendar_day_id
       AND C.calendar_available = 1
       AND L.host_id = H.host_id
       AND H.host_name = "Viajes Eco";
+
 ---------------------06-----------------------
+SELECT DISTINCT H.host_id, H.host_name
+FROM Host H,
+     Listing L
+WHERE H.listing_id = L.listing_id
+GROUP BY L.listing_id
+HAVING COUNT(L.listing_id) = 1;
+
 ---------------------07-----------------------
+SELECT AVG(L1.price) - AVG(L2.price)
+FROM Listing L1,
+     Listing L2,
+     Listing_amenity_map M1,
+     Listing_amenity_map M2,
+WHERE L1.listing_id = M1.listing_id
+      AND M1.amenity_id IN (SELECT A.amenity_id
+                           FROM Amenity A
+                           WHERE A.amenity_name = "Wifi"
+                           OR  A.amenity_name = "Pocket wifi")
+      AND L2.listing_id = M2.listing_id
+      AND M2.amenity_id NOT IN (SELECT A.amenity_id
+                                FROM Amenity A
+                                WHERE A.amenity_name = "Wifi"
+                                OR  A.amenity_name = "Pocket wifi");
 ---------------------08-----------------------
 ---------------------09-----------------------
 ---------------------10-----------------------
