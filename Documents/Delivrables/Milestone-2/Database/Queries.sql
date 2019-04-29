9d68fc83bf4c9bd9737f7fa9fc33d2ea84cf11f0
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-29 22:30:01 +0200

    Add create.sql, queries.sql and schema to milestone 2

--- Documents/Delivrables/Milestone-2/Database/Queries.sql
+++ Documents/Delivrables/Milestone-2/Database/Queries.sql
@@ -65,9 +65,9 @@ FROM Listing L1,
      Listing_amenity_map M2,
 WHERE (L1.listing_id = M1.listing_id)
       AND (M1.amenity_id IN (SELECT A.amenity_id
-                            FROM Amenity A
-                            WHERE A.amenity_name = "Wifi"
-                            OR A.amenity_name = "Pocket wifi"))
+                             FROM Amenity A
+                             WHERE A.amenity_name = "Wifi"
+                             OR A.amenity_name = "Pocket wifi"))
       AND (L2.listing_id = M2.listing_id)
       AND M2.amenity_id NOT IN (SELECT A.amenity_id
                                 FROM Amenity A
@@ -79,29 +79,30 @@ SELECT AVG(L1.price) - AVG(L2.price)
 FROM Listing L1,
      Listing L2
 WHERE (L1.listing_id IN (SELECT L.listing_id
-                        FROM Listing L,
-                             Neighbourhood N,
-                             City C
-                        WHERE L.neighbourhood_id = N.neighbourhood_id
-                        AND N.city_id = C.city_id
-                        AND C.city_name = "Berlin"
-                     ))
+                         FROM Listing L,
+                              Neighbourhood N,
+                              City C
+                         WHERE L.neighbourhood_id = N.neighbourhood_id
+                         AND N.city_id = C.city_id
+                         AND C.city_name = "Berlin"
+                        ))
 AND (L1.beds = 8)
 AND (L2.listing_id IN (SELECT L.listing_id
-                      FROM Listing L
-                           Neighbourhood N,
-                           City C
-                      WHERE L.neighbourhood_id = N.neighbourhood_id
-                      AND N.city_id = C.city_id
-                      AND C.city_name = "Madrid"))
+                       FROM Listing L
+                            Neighbourhood N,
+                            City C
+                       WHERE L.neighbourhood_id = N.neighbourhood_id
+                       AND N.city_id = C.city_id
+                       AND C.city_name = "Madrid"))
 AND (L2.beds = 8);
 
 ---------------------09-----------------------
+--TODO
 
 ---------------------10-----------------------
 SELECT L.listing_id, L.listing_name
 FROM Listing L
 WHERE L.review_score_rating IN (SELECT TOP 10 review_score_rating
-                               FROM Listing
-                               ORDER BY review_score_rating DESC
-                              );
+                                FROM Listing
+                                ORDER BY review_score_rating DESC
+                               );
