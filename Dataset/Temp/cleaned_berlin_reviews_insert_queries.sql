2494e33bff0c88a64fcfc4592fc6e5ba08695e4d
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-29 22:29:09 +0200

    Delete Datasets/Temp

--- Dataset/Temp/cleaned_berlin_reviews_insert_queries.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:252f5f4ac2584fbdd8af74edb1093f93112a7be29112870eaa4e1052f1e0c04b
-size 127010853
--- Dataset/Temp/cleaned_madrid_reviews_insert_queries.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bf1cf2727dec6ec279a55c91f674c8b5e93b9edebdfb76e88b7e9d4737c8171d
-size 186832111
