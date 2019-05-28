00fea4956ff298d54e8cf291224f1272d8060f1b
sophieammann <sophie.ammann@epfl.ch>
2019-05-28 11:43:13 +0200

    query 1 statistics

--- Documents/Delivrables/Milestone-3/optimisation/query1/accelerating_query_01.sql
+++ Documents/Delivrables/Milestone-3/optimisation/query1/accelerating_query_01.sql
@@ -1,2 +1,2 @@
 -- square_feet
-CREATE INDEX square_feet_on_listing ON Listing(square_feet) -- from 0.79sec to 0.042sec
+CREATE INDEX square_feet_on_listing ON Listing(square_feet); -- from 0.79sec to 0.042sec
