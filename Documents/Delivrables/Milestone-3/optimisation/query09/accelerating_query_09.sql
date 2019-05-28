5d4954263e9bfaf40671de7386d1053d430e6ad0
sophieammann <sophie.ammann@epfl.ch>
2019-05-28 17:59:27 +0200

    stats query9

--- Documents/Delivrables/Milestone-3/optimisation/query09/accelerating_query_09.sql
+++ Documents/Delivrables/Milestone-3/optimisation/query09/accelerating_query_09.sql
@@ -1,2 +1,6 @@
 -- room_type_id
 CREATE INDEX room_type_id_on_listing ON Listing(room_type_id) -- doesn't change anything
+--DROP INDEX room_type_id_on_listing ON Listing;
+
+
+--!!!!! this index already exists !!!! no improvement 
