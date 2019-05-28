f2ceb10f2ca831c01f0643f9e9e0efde98a5df56
sophieammann <sophie.ammann@epfl.ch>
2019-05-28 17:34:20 +0200

    query11 stats

--- Documents/Delivrables/Milestone-3/optimisation/query11/accelerating_query_11.sql
+++ Documents/Delivrables/Milestone-3/optimisation/query11/accelerating_query_11.sql
@@ -1,1 +1,2 @@
-CREATE INDEX day_date_day_id_on_day USING BTREE ON Day(day_date, date_id);
+CREATE INDEX day_date_day_id_on_day USING BTREE ON Day(day_date, day_id);
+--DROP INDEX day_date_day_id_on_day ON Day;
