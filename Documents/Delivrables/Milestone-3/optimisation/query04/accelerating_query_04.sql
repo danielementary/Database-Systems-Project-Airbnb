396830a46e0ab838186b787591e681a9ad9b5f18
sophieammann <sophie.ammann@epfl.ch>
2019-05-28 17:15:55 +0200

    format

--- Documents/Delivrables/Milestone-3/optimisation/query04/accelerating_query_04.sql
+++ Documents/Delivrables/Milestone-3/optimisation/query04/accelerating_query_04.sql
@@ -1,6 +1,12 @@
+--only use the first one !!!!
+
 CREATE INDEX host_v_id_descr
 ON Host_verification(host_verification_description(120), host_verification_id);
 --DROP INDEX host_v_id_descr ON Host_verification;
+
+
+
+
 
 
 
