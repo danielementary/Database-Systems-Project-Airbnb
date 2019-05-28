b9eb5ea2c056ca9273c14c5ec630309afddf6c21
Samuel Chassot <samuel.chassot@epfl.ch>
2019-05-28 08:48:46 +0200

    accelerating query 01 and 10 (try 09 but no improvement)

---
+++ Documents/Delivrables/Milestone-3/accelerating_query_09.sql
@@ -0,0 +1,2 @@
+-- room_type_id
+CREATE INDEX room_type_id_on_listing ON Listing(room_type_id) -- doesn't change anything
--- Documents/Delivrables/Milestone-3/accelerating_query_10.sql
+++ Documents/Delivrables/Milestone-3/accelerating_query_10.sql
@@ -1,2 +1,3 @@
--- CREATE INDEX since_on_host USING BTREE ON Host(host_since); -- don't understand why it's longer using this
 CREATE INDEX since_on_host_with_host_id USING BTREE ON Host(host_since, host_id); -- goes from 1min 17sec to 1min 9sec
+
+CREATE INDEX available_on_calendar ON Calendar(calendar_available)
